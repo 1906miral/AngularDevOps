4ecfb3fd1e65652ac91192e8576a899c9d28e93a
1906miral <barhoumimiral88@gmail.com>
2022-11-12 23:29:07 +0100

    jenkins

--- src/app/shared/Service/Stock.service.ts
+++ src/app/shared/Service/Stock.service.ts
@@ -7,7 +7,7 @@ import { HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class StockService {
-   readonly API_URL = 'http://192.168.1.149:8083/SpringMVC/stock';
+   readonly API_URL = 'http://192.168.1.149:8089/SpringMVC/stock';
 
   constructor(private httpClient: HttpClient) { }
 
