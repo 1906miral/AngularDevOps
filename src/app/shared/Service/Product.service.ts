fae7859063be99826289532f6555dcd13ba3fed4
1906miral <barhoumimiral88@gmail.com>
2022-11-13 10:35:57 +0100

    Update Product.service.ts

--- src/app/shared/Service/Product.service.ts
+++ src/app/shared/Service/Product.service.ts
@@ -6,20 +6,20 @@ import { HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class ProductService {
-  readonly API_URL = 'http://192.168.1.149:8089/SpringMVC/produit';
+  //readonly API_URL = 'http://192.168.1.149:8089/SpringMVC/produit';
 
   constructor(private httpClient: HttpClient) { }
   getAllProducts() {
-    return this.httpClient.get(`${this.API_URL}/retrieve-all-produits`)
+    return this.httpClient.get('/url/SpringMVC/produit/retrieve-all-produits')
   }
   addProduct(product : any) {
-    return this.httpClient.post(`${this.API_URL}/add-produit`, product)
+    return this.httpClient.post('/url/SpringMVC/produit/add-produit', product)
   }
   editProduct(product : any){
-    return this.httpClient.put(`${this.API_URL}/modify-produit`, product)
+    return this.httpClient.put('/url/SpringMVC/produit/modify-produit', product)
   }
   deleteProduct(idProduct : any){
-    return  this.httpClient.delete(`${this.API_URL}/remove-produit/${idProduct}`)
+    return  this.httpClient.delete('/url/SpringMVC/produit/remove-produit/${idProduct}')
   }
 
 }
